fd9171d74631b7521eddc0fdd88292aefbc1bf84
herquez <miguelherquez@gmail.com>
2026-05-06 10:31:07 -0600

    Init registry-server

---
+++ registry-server/src/test/java/com/galen/registry_server/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galen.registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
