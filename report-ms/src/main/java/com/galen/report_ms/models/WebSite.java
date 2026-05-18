970b14b8e823664c6af0ada8e3b53cd8da7f20e9
herquez <miguelherquez@gmail.com>
2026-05-18 15:14:31 -0600

    Simplify WebSite model for report payload mapping

--- report-ms/src/main/java/com/galen/report_ms/models/WebSite.java
+++ report-ms/src/main/java/com/galen/report_ms/models/WebSite.java
@@ -1,16 +1,16 @@
 package com.galen.report_ms.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WebSite implements Serializable {
-    private Long id;
-
     private String name;
-
-    private Category category;
-
-    private String description;
 }
